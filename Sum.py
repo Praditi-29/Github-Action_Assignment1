9902f4b47d1e7144e3e898a38654c9a1365e2588
Praditi-29 <Praditi.Sharma@cloudEQ.com>
2023-01-09 13:36:51 +0530

    changes

--- Sum.py
+++ Sum.py
@@ -1,7 +1,8 @@
-import sys
-
-a=int(sys.argv[0])
-b=int(sys.argv[1])
+import os
+a = os.environ["Number-1"]
+b= os.environ["Number-2"]
+# a=int(sys.argv[0])
+# b=int(sys.argv[1])
 
 
 print('Sum =' ,a+b)
