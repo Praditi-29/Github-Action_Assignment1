141e92143537b6406207f26fae2f1eb6696f9f18
Praditi-29 <Praditi.Sharma@cloudEQ.com>
2023-01-09 13:54:35 +0530

    Final

--- Sub.py
+++ Sub.py
@@ -1,5 +1,5 @@
 import os
-start= os.environ["num1"]
-end= os.environ["num2"]
+start= int(os.environ["num1"]) 
+end= int(os.environ["num2"]) 
 
 print("sub : ",start-end)
--- Sum.py
+++ Sum.py
@@ -1,4 +1,4 @@
 import os
-a = os.environ["num1"]
-b= os.environ["num2"]
+a =int(os.environ["num1"]) 
+b= int(os.environ["num2"]) 
 print('Sum =' ,a+b)
