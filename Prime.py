742c82ac52a39a653271d53d4fbfd98dd4c72b09
Praditi-29 <Praditi.Sharma@cloudEQ.com>
2023-01-09 13:40:24 +0530

    Final Changes

--- Prime.py
+++ Prime.py
@@ -1,12 +1,11 @@
-import sys
-
-# start=int(sys.argv[1])
-# end=int(sys.argv[2])
+import os
+start= os.environ["num1"]
+end= os.environ["num2"]
 print('Prime Numbers are :')
-# for num in range(start, end + 1):
-#     if num > 1:
-#         for i in range(2, num):
-#             if (num % i) == 0:
-#                 break
-#             else:
-#                 print(num)
+for num in range(start, end + 1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+            else:
+                print(num)
