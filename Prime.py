72cfc1a6c902797bd3483c2cccc6867304c033c8
Praditi-29 <Praditi.Sharma@cloudEQ.com>
2023-01-09 14:17:08 +0530

    Final

--- Prime.py
+++ Prime.py
@@ -1,11 +1,15 @@
 import sys
+
 start=int(sys.argv[1])
 end=int(sys.argv[2])
-print("Prime numbers are:")
+
+print ("The Prime Numbers in the range are: ")  
 for num in range(start, end + 1):
    if num > 1:
        for i in range(2, num):
-        if (num % i) == 0:
-            break
-        else:
-            print(num)
+           if (num % i) == 0:
+              break
+
+       else:
+
+          print(num)
